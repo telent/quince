720751aad4f3decb9af7b042bfe8c9f02d524f8b
Daniel Barlow <dan@telent.net>
2013-12-10 22:51:51 +0000

    read a maildir, return a seq contianing plausible filenames

---
+++ src/quince/maildir.clj
@@ -0,0 +1,14 @@
+(ns quince.maildir
+  (:use [midje.sweet]))
+
+(defn maildir-files-seq [dir]
+  (let [files (file-seq (clojure.java.io/file dir))]
+    (filter #(and (.isFile %)
+                  (re-find #"^([0-9]+)\.[MP]([0-9]+)" (.getName %)))
+            files)))
+
+(facts "maildir-files-seq"
+       (let [dir (System/getProperty "user.dir")
+             s (maildir-files-seq (java.io.File. dir "fixtures/nnml/"))]
+         (fact "returns regular files"
+               s => (partial every? #(.isFile %)))))
