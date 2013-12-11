fc010ae4e3e0aff0b173335ff13d36a965911f4d
Daniel Barlow <daniel.barlow@simplybusiness.co.uk>
2013-12-11 12:33:52 +0000

    reformat comment

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "http://example.com/FIXME"
   :main quince.core
 
-  ;; The GNU Affero General Public License is a modified version of
-  ;; the ordinary GNU GPL version 3. It has one added requirement: if
-  ;; you run the program on a server and let other users communicate
-  ;; with it there, your server must also allow them to download the
-  ;; source code corresponding to the program that it's running. If
-  ;; what's running there is your modified version of the program, the
-  ;; server's users must get the source code as you modified it.
-
   :license {:name "GNU Affero General Public License"
+            ;; "... a modified version of the ordinary GNU GPL version
+            ;; 3. It has one added requirement: if you run the program
+            ;; on a server and let other users communicate with it
+            ;; there, your server must also allow them to download the
+            ;; source code corresponding to the program that it's
+            ;; running. If what's running there is your modified
+            ;; version of the program, the server's users must get the
+            ;; source code as you modified it."
             :url "https://www.gnu.org/licenses/agpl-3.0.html"}
 
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
