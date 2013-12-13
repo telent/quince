147d4e2192b7eb2f8a3e6471a9f318256602a593
Daniel Barlow <dan@telent.net>
2013-12-13 11:56:25 +0000

    make our btree node a defrecord, in the hope it will make it better for use with reducers

--- src/quince/btree.clj
+++ src/quince/btree.clj
@@ -1,8 +1,17 @@
 (ns quince.btree
   (:use [midje.sweet]))
 
+
+(defrecord Tree [key value left right]
+  )
+
+(def empty-tree (map->Tree {}))
+
+(defn empty-tree? [tree]
+  (or (nil? tree) (nil? (:key tree))))
+
 (defn height [tree]
-  (if (nil? tree)
+  (if (empty-tree? tree)
     0
     (+ 1 (max (height (:left tree)) (height (:right tree))))))
 
@@ -29,11 +38,11 @@
      (let [grandchild (:right left)
            inner-left (:left grandchild)
            inner-right (:right grandchild)]
-       {:key (:key grandchild) :value (:value grandchild)
-        :left (assoc left :right inner-left)
-        :right (assoc tree :left inner-right)})
+       (Tree. (:key grandchild) (:value grandchild)
+              (assoc left :right inner-left)
+              (assoc tree :left inner-right)))
 
-    ;; if right-heavy, similar but opposite
+     ;; if right-heavy, similar but opposite
      (and (>= (imbalance tree) 2)
           (>= (imbalance right) 1))
      (let [inside (:left right)]
@@ -45,9 +54,9 @@
      (let [grandchild (:left right)
            inner-left (:left grandchild)
            inner-right (:right grandchild)]
-       {:key (:key grandchild) :value (:value grandchild)
-        :left (assoc tree :right inner-left)
-        :right (assoc right :left inner-right)})
+       (Tree. (:key grandchild) (:value grandchild)
+              (assoc tree :right inner-left)
+              (assoc right :left inner-right)))
 
      ;; otherwise don't know how to rebalance
      true (assert nil))))
@@ -56,10 +65,12 @@
   ([tree [key value]] (tree-insert tree key value))
   ([tree key value]
      (let [tree
-           (cond (nil? tree)
-                 {:left nil :right nil :key key :value value}
+           (cond (empty-tree? tree)
+                 (Tree. key value nil nil)
+
                  (<= (compare key (:key tree)) 0)
                  (assoc tree :left (tree-insert (:left tree) key value))
+
                  (> (compare key (:key tree)) 0)
                  (assoc tree :right (tree-insert (:right tree) key value))
                  )
@@ -72,35 +83,36 @@
 
 (fact "the height of a tree is the max number of nodes between the root and any leaf"
       (let [tree (reduce (fn [tree key] (tree-insert tree key {}))
-                         nil [     8
-                                4     12
-                               2 6  10  14])]
+                         empty-tree
+                         [     8
+                            4     12
+                          2 6  10  14])]
         (height tree)) => 3)
 
-(fact "adding a key/value to nil makes a tree with one node"
-      (tree-insert nil 42 [1 2 3 4]) =>
+(fact "adding a key/value to the empty tree makes a tree with one node"
+      (tree-insert empty-tree 42 [1 2 3 4]) =>
       {:left nil :right nil :key 42 :value [1 2 3 4]})
 
 (fact "adding an earlier node makes it the left child"
-      (let [node1 (tree-insert nil 42 [1 2 3 4])]
+      (let [node1 (tree-insert empty-tree 42 [1 2 3 4])]
         (tree-insert node1 10 [1 1 1]))
       => #(= (:key (:left %)) 10))
 
 (fact "adding a later node makes it the right child"
-      (let [node1 (tree-insert nil 42 [1 2 3 4])]
+      (let [node1 (tree-insert empty-tree 42 [1 2 3 4])]
         (tree-insert node1 64 [1 1 1]))
       => #(= (:key (:right %)) 64))
 
 (facts "about imbalance"
        (let [left (reduce (fn [tree key] (tree-insert tree key {}))
-                          nil [8 4 12 2 6  10  14 1])
+                          empty-tree [8 4 12 2 6  10  14 1])
              right (reduce (fn [tree key] (tree-insert tree key {}))
-                           nil [-2 -3 -1])]
+                           empty-tree [-2 -3 -1])]
          (imbalance {:left left :right right :key 0 :value :hello}))
        => -2)
 
 (defn tree-insert-seq [seq]
-  (reduce (fn [tree key] (tree-insert tree key {})) nil seq))
+  (reduce (fn [tree key] (tree-insert tree key {})) empty-tree seq))
 (defn balanced? [tree] (< -2 (imbalance tree) 2))
 
 (fact "tree-insert never creates a tree with imbalance greater than 1"
@@ -122,7 +134,8 @@
 
 (facts "tree-lookup"
       (let [nodes (map #(list %1 %2) (range 1 20) (reverse (range 1 20)))
-            tree (reduce (fn [tree [k v]] (tree-insert tree k v)) nil nodes)]
+            tree (reduce (fn [tree [k v]] (tree-insert tree k v))
+                         empty-tree nodes)]
         (fact "finds the first value associated with the provided key"
               (tree-lookup tree 5) => 15
               (tree-lookup tree 1) => 19
